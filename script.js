0d2f40cf4afdc30c5e97aa523a6cc883dd21f28d
fugbot <18075133+fugbot@users.noreply.github.com>
2024-02-15 10:12:15 +0100

    Add playGame function to play 5 rounds

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("hello world 2");
+// console.log("hello world 2");
 
 function getComputerChoice(){
     const options = ["Rock", "Paper", "Scissors"];
@@ -11,20 +11,63 @@ function playRound(playerSelection, computerSelection){
 
     console.log("Player: " + playerSelection + " Computer: " + computerSelection);
     if (playerSelection === computerSelection){
-        alert('Tie! You both selected ' + playerSelection);
+        return "Tie! You both selected " + playerSelection;
     }
     else if(playerSelection === "scissors" && computerSelection == "rock" || 
             playerSelection === "rock" && computerSelection == "paper" ||
             playerSelection === "paper" && computerSelection == "scissors"){
-        alert('You Lose! ' + computerSelection + " beats " + playerSelection);
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
     } else {
-        alert('You Win! ' + playerSelection + " beats " + computerSelection);
+        return "You Win! " + playerSelection + " beats " + computerSelection;
     }
+}
+
+function playGame(playerSelection, computerSelection){
+    
+    //start from 0 games played
+    //play round
+    //check if win/lose
+    //add 1 game to win/lose
+    //add 1 to total
+    //when 5 games total - end 
+    //let totalGamesPlayed = 0;
+    let wonGames = 0;
+    let tiedGames = 0;
+    let lostGames = 0;
+    let maxGames = 5;    
+
+    for(let totalGamesPlayed = 1; totalGamesPlayed <= maxGames; totalGamesPlayed++){
+        const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+        const computerSelection = getComputerChoice().toLowerCase();
+        let result = playRound(playerSelection, computerSelection);
+        if(result.includes("Lose")){
+            lostGames++;
+        }else if(result.includes("Win")){
+            wonGames++;
+        }else if(result.includes("Tie")){
+            tiedGames++;
+        }
+        console.log(result);
+        console.log("Number of games played: " + totalGamesPlayed);
+        console.log("Number of wins: " + wonGames);
+        console.log("Number of loses: " + lostGames);
+        console.log("Number of ties: " + tiedGames);
+    }
+
+    if(wonGames>lostGames){
+        console.log("You won the game with " + wonGames + "!");
+    }else {
+        console.log("You lost the game.")
+    }
+    
+
 }
 
 getComputerChoice();
 
-const playerSelection = "rOck".toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
+// const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+// const computerSelection = getComputerChoice().toLowerCase();
 
-playRound(playerSelection, computerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+
+playGame();

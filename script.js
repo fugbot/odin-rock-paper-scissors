ebac8d6b0c7b0bc6072d5b9cea88dcdccaf21c29
fugbot <18075133+fugbot@users.noreply.github.com>
2024-02-26 09:34:46 +0000

    Added reset button and results scorecard

--- script.js
+++ script.js
@@ -3,7 +3,41 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
-const btns = document.querySelectorAll("button");
+const btns = document.querySelectorAll("button.choice");
+const resetBtn = document.querySelector("#reset");
+const resultsList = document.querySelector("ul.resultsList");
+const resultDiv = document.querySelector(".results");
+
+let playerWonGames = 0;
+let playerLostGames = 0;
+let compWonGames = 0;
+let compLostGames = 0;
+
+let tiedGames = 0;
+let maxWonGames = 5;
+let totalGamesPlayed = 0;
+
+const scorecard = document.querySelector(".scorecard");
+    const win = document.querySelector(".winScore");
+    const lose = document.querySelector(".loseScore");
+    const tie = document.querySelector(".tieScore");
+    const totalGames = document.querySelector(".gameScore");
+
+    const winItem = document.createElement("div");
+    const loseItem = document.createElement("div");
+    const tieItem = document.createElement("div");
+    const totalItem = document.createElement("div");
+
+function createEmptyScoreCard(){
+    //create score card value
+    win.appendChild(winItem).textContent = playerWonGames;
+    lose.appendChild(loseItem).textContent = playerLostGames;
+    tie.appendChild(tieItem).textContent = tiedGames;
+    totalGames.appendChild(totalItem).textContent = totalGamesPlayed;
+}
+
+createEmptyScoreCard();
+
 
 btns.forEach((btn) => {
     /* btn.addEventListener("click", () => {
@@ -13,17 +47,80 @@ btns.forEach((btn) => {
     
     }); */
 
-    btn.addEventListener("click", () => {
-        const playerSelection = btn.className;
-        const computerSelection = getComputerChoice();
+    btn.addEventListener("click", function playChoice () {
+        if (maxWonGames<=playerWonGames||maxWonGames<=compWonGames){
+            //alert("again?");
+            const endResult = document.createElement("div");
+             if(playerWonGames > compWonGames){
+                resultDiv.appendChild(endResult).textContent="You win! Do you want to play again? Click Reset button or a move button to start again.";
+                
+                //resultDiv.removeChild(endResult);
+             }else {
+                resultDiv.appendChild(endResult).textContent="You lost! Do you want to play again? Click Reset button or a move button to start again."; 
+                
+                //resultDiv.removeChild(endResult);
+            }
+            //btn.removeEventListener("click", playChoice);
+            resetScore();
+            //resultDiv.removeChild(endResult);
+        } else {
+            const playerSelection = btn.id;
+            const computerSelection = getComputerChoice();
+
+            
+            
+
+            const result = playRound(playerSelection, computerSelection);
+            if(result.includes("Lose")){
+                playerLostGames++;
+                compWonGames++;
+            }else if(result.includes("Win")){
+                playerWonGames++;
+                compLostGames++
+            }else if(result.includes("Tie")){
+                tiedGames++;
+            }
+            totalGamesPlayed++;
+
+            printRoundResult(result);
+
+            //print scorecard results
+            printRoundScorecard(playerWonGames, playerLostGames, tiedGames, totalGamesPlayed);
+
+
+        }
+                 
         // console.log("player" + typeof playerSelection);
         // console.log("comp" + typeof computerSelection);
-        playRound(playerSelection, computerSelection);
+        //const result = playRound(playerSelection, computerSelection);
+        //const result = playUntilFiveWon(playerSelection, computerSelection);
+
+        
+
+
     });
 });
 
-const results = document.querySelector(".results");
+resetBtn.addEventListener("click", resetScore);
 
+
+function printRoundResult(result){
+    
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+
+    //give results as text to results list
+    listItem.appendChild(listText);
+    listText.textContent = result;
+    resultsList.appendChild(listItem);
+}
+
+function printRoundScorecard(win, lose, tie, total){
+    winItem.textContent = win;
+    loseItem.textContent = lose;
+    tieItem.textContent = tie;
+    totalItem.textContent = total;
+}
 
 function getComputerChoice(){
     const options = ["rock", "paper", "scissors"];
@@ -36,18 +133,63 @@ function playRound(playerSelection, computerSelection){
 
     console.log("Player: " + playerSelection + " Computer: " + computerSelection);
     if (playerSelection === computerSelection){
-        //return "Tie! You both selected " + playerSelection;
-        console.log("Tie! You both selected " + playerSelection);
+        return "Tie! You both selected " + playerSelection;
+        //console.log("Tie! You both selected " + playerSelection);
     }
     else if(playerSelection === "scissors" && computerSelection == "rock" || 
             playerSelection === "rock" && computerSelection == "paper" ||
             playerSelection === "paper" && computerSelection == "scissors"){
-        //return "You Lose! " + computerSelection + " beats " + playerSelection;
-        console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+        //console.log("You Lose! " + computerSelection + " beats " + playerSelection);
     } else {
-        //return "You Win! " + playerSelection + " beats " + computerSelection;
-        console.log("You Win! " + playerSelection + " beats " + computerSelection);
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+        //console.log("You Win! " + playerSelection + " beats " + computerSelection);
     }
+}
+
+function resetScore(){
+    playerWonGames = 0;
+    playerLostGames = 0;
+    compWonGames = 0;
+    compLostGames = 0;
+
+    tiedGames = 0;
+    totalGamesPlayed = 0;
+
+    createEmptyScoreCard();
+    
+    //remove round result list
+    resultsList.innerHTML = "";
+   // endResult.innerHTML = "";
+    //resultDiv.removeChild(endResult);
+   
+};
+
+//does not work
+function playUntilFiveWon(){
+    let playerWonGames = 0;
+    let playerLostGames = 0;
+    let compWonGames = 0;
+    let compLostGames = 0;
+    
+    let tiedGames = 0;
+    let maxWonGames = 5;
+    let totalGamesPlayed;
+
+    for(totalGamesPlayed = 1; (maxWonGames<=playerWonGames)||(maxWonGames<=compWonGames); totalGamesPlayed++){
+        let gameResult = playRound(player, computer);
+        if(result.includes("Lose")){
+            playerLostGames++;
+            compWonGames++;
+        }else if(result.includes("Win")){
+            playerWonGames++;
+            compLostGames++
+        }else if(result.includes("Tie")){
+            tiedGames++;
+        }
+    }
+    return "Player: " + playerWonGames + "total: " + totalGamesPlayed;
+    
 }
 
 /* function playGame(playerSelection, computerSelection){
@@ -84,11 +226,3 @@ function playRound(playerSelection, computerSelection){
 
 } */
 
-getComputerChoice();
-
-// const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-// const computerSelection = getComputerChoice().toLowerCase();
-
-// console.log(playRound(playerSelection, computerSelection));
-
-//playGame();
